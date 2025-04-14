46570574e3fe3a1d7ceb3f8b0e3b433bde7f3d04
zayminmaw <zayminmaw77@gmail.com>
2025-04-14 22:07:35 +0700

    feat: code clean up

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,4 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
@@ -63,9 +62,6 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
       </main>
       <footer className={styles.footer}>
         <a
