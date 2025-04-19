ba506838a146f47d3d3f2b594f8c5bfd1f596f28
zayminmaw <zayminmaw77@gmail.com>
2025-04-19 14:15:30 +0700

    feat: added default semantic color map

---
+++ packages/dynawind/src/constants/semanticColor.ts
@@ -0,0 +1,17 @@
+export default {
+  primary: "var(--color-primary)",
+  "primary-foreground": "var(--color-primary-foreground)",
+  secondary: "var(--color-secondary)",
+  "secondary-foreground": "var(--color-secondary-foreground)",
+  background: "var(--color-background)",
+  foreground: "var(--color-foreground)",
+  muted: "var(--color-muted)",
+  "muted-foreground": "var(--color-muted-foreground)",
+  accent: "var(--color-accent)",
+  "accent-foreground": "var(--color-accent-foreground)",
+  destructive: "var(--color-destructive)",
+  "destructive-foreground": "var(--color-destructive-foreground)",
+  border: "var(--color-border)",
+  input: "var(--color-input)",
+  ring: "var(--color-ring)",
+};
--- packages/dynawind/src/index.ts
+++ packages/dynawind/src/index.ts
@@ -1,1 +1,2 @@
 export * from "./utils/cssVars";
+export * from "./constants";
