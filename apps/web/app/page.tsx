2ebe561934ade1a508eb9a52a6397d7ecc236fdc
zayminmaw <zayminmaw77@gmail.com>
2025-04-19 14:03:13 +0700

    feat: setup tailwind

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,5 +3,9 @@ import { generateCSSVariables } from "dynawind";
 // mock https://mocki.io/v1/3d4be87a-17f9-41f4-a232-7401072eb6b7
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="bg-red-300">
+      <div className="text-xl text-black">hello</div>
+    </div>
+  );
 }
