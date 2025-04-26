b7c9f7817fb379f936c924ac3114f1ed1126264e
zayminmaw <zayminmaw77@gmail.com>
2025-04-26 17:57:43 +0700

    feat: added usage

--- apps/docs/app/layout.config.tsx
+++ apps/docs/app/layout.config.tsx
@@ -11,14 +11,14 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
+        {/* <svg
           width="24"
           height="24"
           xmlns="http://www.w3.org/2000/svg"
           aria-label="Logo"
         >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
+        </svg> */}
         dynawind
       </>
     ),
